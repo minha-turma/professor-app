d161968c14801dc0ad42952020a8ddb9e617d0c0
Carlos Olimpio <olimpio@motorola.com>
2019-02-20 08:48:04 -0300

    Ajustes no Layout

--- app/src/main/java/minhaturma/ufrpe/br/minhaturma/reports/items/PresenceReportFragment.java
+++ app/src/main/java/minhaturma/ufrpe/br/minhaturma/reports/items/PresenceReportFragment.java
@@ -1,11 +1,20 @@
 package minhaturma.ufrpe.br.minhaturma.reports.items;
 
 
+import android.graphics.Color;
 import android.os.Bundle;
+import android.support.annotation.Nullable;
 import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+
+import com.github.mikephil.charting.charts.BarChart;
+import com.github.mikephil.charting.data.BarData;
+import com.github.mikephil.charting.data.BarDataSet;
+import com.github.mikephil.charting.data.BarEntry;
+
+import java.util.ArrayList;
 
 import minhaturma.ufrpe.br.minhaturma.R;
 
@@ -32,6 +41,25 @@ public class PresenceReportFragment extends Fragment {
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
         return inflater.inflate(R.layout.fragment_presence_report, container, false);
+    }
+
+    @Override
+    public void onViewCreated(View view, @Nullable Bundle savedInstanceState) {
+        super.onViewCreated(view, savedInstanceState);
+        BarChart barChart = view.findViewById(R.id.barchart);
+
+        ArrayList<BarEntry> entries = new ArrayList<>();
+        entries.add(new BarEntry(4f, 0.5f));
+        entries.add(new BarEntry(6f, 1));
+
+        BarDataSet dataSet1 = new BarDataSet(entries, "Presenças");
+        dataSet1.setColor(Color.BLUE);
+        BarDataSet dataSet2 = new BarDataSet(entries, "Faltas");
+        dataSet2.setColor(Color.RED);
+
+        BarData data = new BarData(dataSet1, dataSet2);
+
+        barChart.setData(data);
     }
 
     public String getTitle() {
